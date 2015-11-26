d20a66d7d38ba66bd2fabfb988cca9894fdfead0
BestianRU <oleg.a.smirnov@gmail.com>
2015-11-27 00:51:18 +0300

    2015.11.27.01.00.00

--- SBMConnect/LDAP.go
+++ SBMConnect/LDAP.go
@@ -2,6 +2,7 @@ package SBMConnect
 
 import (
 	"log"
+	//"strings"
 
 	// LDAP
 	"github.com/go-ldap/ldap"
@@ -12,15 +13,17 @@ import (
 var LDAPCounter = int(0)
 
 type LDAP struct {
-	D *ldap.Conn
+	CS int
+	D  *ldap.Conn
 }
 
 func (_s *LDAP) Init(conf SBMSystem.ReadJSONConfig) int {
 	var (
 		attemptCounter = int(0)
 		err            error
-		//l   *ldap.Conn
 	)
+
+	_s.CS = -1
 
 	for {
 		if attemptCounter > len(conf.Conf.LDAP_URL)*2 {
@@ -53,9 +56,122 @@ func (_s *LDAP) Init(conf SBMSystem.ReadJSONConfig) int {
 		return -1
 	}
 
+	_s.CS = 0
 	return 0
 }
 
+func (_s *LDAP) InitS(rLog SBMSystem.LogFile, user, password, server string) int {
+	var err error
+
+	_s.CS = -1
+
+	log.Printf("LDAP Init SRV ***** Trying connect to server %s with login %s.", server, user)
+
+	_s.D, err = ldap.Dial("tcp", server)
+	if err != nil {
+		log.Printf("LDAP::Dial() error: %v\n", err)
+		return -1
+	}
+
+	//L.Debug()
+
+	err = _s.D.Bind(user, password)
+	if err != nil {
+		log.Printf("LDAP::Bind() error: %v\n", err)
+		return -1
+	}
+
+	log.Printf("LDAP Init SRV ***** Success! Connected to server %s with login %s.", server, user)
+
+	_s.CS = 0
+	return 0
+}
+
+func (_s *LDAP) CheckGroupMember(rLog SBMSystem.LogFile, user, group, baseDN string) int {
+	const (
+		recurs_count = 10
+	)
+
+	log.Printf("LDAP CheckGroupMember...")
+
+	userDN := _s._getBaseDN(rLog, user, baseDN)
+	groupDN := _s._getBaseDN(rLog, group, baseDN)
+
+	if userDN == "" || groupDN == "" {
+		return -1
+	}
+
+	if _s._checkGroupMember(rLog, userDN, groupDN, baseDN, 1) == 0 {
+		return 0
+	} else {
+		return _s._checkGroupMember(rLog, userDN, groupDN, baseDN, recurs_count)
+	}
+
+	return -1
+}
+
+func (_s *LDAP) _checkGroupMember(rLog SBMSystem.LogFile, userDN, groupDN, baseDN string, recurse_count int) int {
+	var (
+		uattr  = []string{"memberOf"}
+		result = int(-1)
+	)
+
+	if userDN == "" || groupDN == "" {
+		return -1
+	}
+
+	if recurse_count <= 0 {
+		return -1
+	}
+
+	lsearch := ldap.NewSearchRequest(userDN, 0, ldap.NeverDerefAliases, 0, 0, false, "(objectclass=*)", uattr, nil)
+	sr, err := _s.D.Search(lsearch)
+	if err != nil {
+		log.Printf("LDAP::Search() error: %v\n", err)
+	}
+
+	if len(sr.Entries) > 0 {
+		for _, entry := range sr.Entries {
+			for _, attr := range entry.Attributes {
+				if attr.Name == "memberOf" {
+					for _, x := range attr.Values {
+						if groupDN == x {
+							return 0
+						} else {
+							if x != userDN {
+								result = _s._checkGroupMember(rLog, x, groupDN, baseDN, recurse_count-1)
+								if result == 0 {
+									return 0
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func (_s *LDAP) _getBaseDN(rLog SBMSystem.LogFile, search, basedn string) string {
+	var uattr = []string{"dn"}
+
+	lsearch := ldap.NewSearchRequest(basedn, 2, ldap.NeverDerefAliases, 0, 0, false, search, uattr, nil)
+	sr, err := _s.D.Search(lsearch)
+	if err != nil {
+		log.Printf("LDAP::Search() error: %v\n", err)
+	}
+
+	if len(sr.Entries) > 0 {
+		for _, entry := range sr.Entries {
+			return entry.DN
+		}
+	}
+	return ""
+}
+
 func (_s *LDAP) Close() {
-	_s.D.Close()
+	if _s.CS != -1 {
+		_s.D.Close()
+	}
 }
